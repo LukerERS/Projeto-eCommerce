111d1050b0d17462792605d1cb8f72fab415582e
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-09-26 15:51:07 -0300

    Criando Classe Model Product

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Ecommerce.Models 
+{
+    public class Product 
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+    }
+}
