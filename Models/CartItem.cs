40017648e5ed63ebaba64f4bcc74c7c35cad324a
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-09-26 16:16:52 -0300

    Criando Classe Modelo CartItem e Order

---
+++ Models/CartItem.cs
@@ -0,0 +1,18 @@
+using System;
+
+//modelo do produto adicionado no carrinho de compras
+namespace Ecommerce.Models 
+{
+    public class CartItem 
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System;
+
+//modelo do pedido 
+namespace Ecommerce.Models 
+{
+    public class Order 
+    {
+        public Order () => CreatedAt = DateTime.Now; 
+
+        public int Id { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public List<CartItem> products { get; set; }
+
+        public DateTime CreatedAt {get; set; }
+    }
+}
