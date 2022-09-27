faffe98fb83927ae45b780c1a2fd2387f4152be4
aifojeruijg <101339217+aifojeruijg@users.noreply.github.com>
2022-09-27 18:55:24 -0300

    Update DataContext.cs

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -5,5 +5,7 @@ namespace Ecommerce.Models
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
