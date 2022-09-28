9aceeb9ff34599a8e4b83a43bbc314210b706242
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-09-28 19:29:43 -0300

    Update model CartItem

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -7,13 +7,13 @@ namespace Ecommerce.Models
     {
         public int Id { get; set; }
 
-        public int ProductId { get; set; }
+        public int IdProduto { get; set; }
 
-        public string Name { get; set; }
+        public string Nome { get; set; }
 
-        public int Quantity { get; set; }
+        public int Quantidade { get; set; }
 
-        public double Price { get; set; }
+        public double Preco { get; set; }
         
     }
 }
