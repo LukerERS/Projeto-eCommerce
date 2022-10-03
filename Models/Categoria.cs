31a973ebabe5a86eac9592e7c4474c5e4072b912
LukerERS <101291918+LukerERS@users.noreply.github.com>
2022-10-03 15:34:33 -0300

    Relacionamento corrigido

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,12 +1,11 @@
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace Ecommerce.Models
 {
     public class Categoria
     {
+        [Key()]
         public int Id {get;set;}
         public string Tipo {get;set;}
-
-        public ICollection<Produto> Produtos {get;set;}
     }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,19 +1,19 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Ecommerce.Models 
 {
     public class Produto 
-    {
+    {   
+        [Key()]
         public int Id { get; set; }
-
+        [ForeignKey("Categoria")]
+        public int CategoriaID {get;set;}
+        public virtual Categoria Categoria {get; set;}
         public string Nome { get; set; }
-
         public string Descricao { get; set; }
-
-        public double Preco { get; set; }
-        //Relação de Um para Muitos de produtos e categoria.
-        public Categoria Categorias {get;set;}
-        
+        public double Preco { get; set; } 
     }
 }
