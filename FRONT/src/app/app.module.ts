1fc53da9c4bcac696e139431aecb7181ab9cb25b
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-11-22 11:48:41 -0300

    Inicio front

---
+++ FRONT/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastrarClienteComponent } from './components/pages/cadastrar-cliente/cadastrar-cliente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastrarClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
