d698a0f901abaf2ff766f90ac5a79efb2d090761
LukerERS <101291918+LukerERS@users.noreply.github.com>
2022-11-23 13:29:00 -0300

    Novos Modelos

---
+++ FRONT/src/app/models/Produto.ts
@@ -0,0 +1,9 @@
+import { Categoria } from "./Categoria";
+export interface Produto{
+    id?:Number;
+    categoriaID:Number;
+    categoria:Categoria;
+    nome:String;
+    descricao:String;
+    preco:Number;
+}
