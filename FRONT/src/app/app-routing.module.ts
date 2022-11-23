16f2cc6ac260f76f2f2511650e1c041ae662620f
LukerERS <101291918+LukerERS@users.noreply.github.com>
2022-11-23 15:57:17 -0300

    add links

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-
+import { CadastrarCategoriaComponent } from './components/pages/cadastrar-categoria/cadastrar-categoria.component';
 import { CadastrarClienteComponent } from './components/pages/cadastrar-cliente/cadastrar-cliente.component';
 import { CadastrarProdutoComponent } from './components/pages/cadastrar-produto/cadastrar-produto.component';
 
@@ -14,7 +14,12 @@ const routes: Routes = [
   {
     path: "pages/cliente/cadastrar",
     component: CadastrarClienteComponent,
+  },
+  {
+    path:"pages/categoria/cadastrar",
+    component: CadastrarCategoriaComponent
   }
+
 ]
 
 @NgModule({
