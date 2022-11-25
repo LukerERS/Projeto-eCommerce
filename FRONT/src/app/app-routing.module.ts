039141f5450607efbcdf6ff12c12941505f1ba16
LukerERS <101291918+LukerERS@users.noreply.github.com>
2022-11-25 13:21:19 -0300

    novos links

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarCategoriaComponent } from './components/pages/cadastrar-categoria/cadastrar-categoria.component';
 import { CadastrarClienteComponent } from './components/pages/cadastrar-cliente/cadastrar-cliente.component';
 import { CadastrarProdutoComponent } from './components/pages/cadastrar-produto/cadastrar-produto.component';
+import { ListarCategoriaComponent } from './components/pages/listar-categoria/listar-categoria.component';
 import { ListarClienteComponent } from './components/pages/listar-clientes/listar-cliente.component';
 
 const routes: Routes = [
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: "pages/cliente/listar",
     component: ListarClienteComponent
+  },
+  {
+    path: "pages/categoria/listar",
+    component: ListarCategoriaComponent
   }
 
 ]
