a490d382f8451cf0298d412b536d0e1237e2bbdd
LukerERS <101291918+LukerERS@users.noreply.github.com>
2022-11-25 13:22:07 -0300

    update categoria

---
+++ FRONT/src/app/components/pages/listar-categoria/listar-categoria.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/models/Categoria';
+
+@Component({
+  selector: 'app-listar-categoria',
+  templateUrl: './listar-categoria.component.html',
+  styleUrls: ['./listar-categoria.component.css']
+})
+export class ListarCategoriaComponent implements OnInit {
+
+  categorias!: Categoria[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Categoria[]>("https://localhost:5001/api/categoria/listar")
+    .subscribe({
+      next:(categorias)=>{
+        this.categorias = categorias;
+      }
+    });
+  }
+
+}
